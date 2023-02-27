abbdceb17906511a5e47d00fd979ad9ceeb0eb0e
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-02-27 14:53:42 +0100

    finishing styling login

--- src/components/login/webLogin/loginModal/LoginModal.jsx
+++ src/components/login/webLogin/loginModal/LoginModal.jsx
@@ -1,4 +1,5 @@
 import "./loginModal.css";
+import { FaGoogle } from "react-icons/fa";
 
 const LoginModal = () => {
   return (
@@ -6,18 +7,34 @@ const LoginModal = () => {
       <div className="loginModal-form">
         <h2>Sign In</h2>
         <div className="loginModal-inputBox">
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Type here your email"
+            id="loginModal-inputBox-input1"
+          />
           <span>Email or Username </span>
-          <i></i>
+          <hr />
         </div>
         <div className="loginModal-inputBox">
-          <input type="password" required />
+          <input
+            type="password"
+            placeholder="Type here your email"
+            id="loginModal-inputBox-input2"
+          />
+          <span>Password </span>
+          <hr />
         </div>
         <div className="loginModal-links">
-          <br></br> <a href=""></a>
-          <a href="">Sing In</a>
+          <br />
+          <br /> <a href="">Login</a>
+          <a href="">Sing Up</a>
         </div>
-        <input type="submit" value="Login" className="loginModal-submit" />
+        <div className="loginModal-google">
+          <div className="loginModal-google-inset">
+            <FaGoogle className="loginModal-google-inset-icon" />
+            <div>Login with Google</div>
+          </div>
+        </div>
       </div>
     </div>
   );
