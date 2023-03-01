c68cdf8233fc9a1d77fe34e4854c4d0bcd41ff1a
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-01 19:04:50 +0100

    adjust water results

--- src/components/home/brew/ingredients/water/adjustedWaterResults/AdjustedWaterResults.jsx
+++ src/components/home/brew/ingredients/water/adjustedWaterResults/AdjustedWaterResults.jsx
@@ -1,12 +1,13 @@
-import "./adjustedWaterResults.css";
 import "../waterSourceData/waterSourceData.css";
+import "./adjustedWaterResults.css";
+
 import { RiCloseLine } from "react-icons/ri";
 const AdjustedWaterResults = ({ setModal }) => {
   return (
     <div className="waterSouceData">
-      <div className="waterSouceData-modal">
+      <div className="waterSouceData-modal adjustedWaterResults">
         <div className="waterSouceData-modal-top">
-          <h4 className="mt-2">Souce Water Data</h4>
+          <h4 className="mt-2">Adjusted Water Results</h4>
           <div
             className="waterSouceData-close"
             onClick={() => setModal("closed")}
@@ -15,55 +16,87 @@ const AdjustedWaterResults = ({ setModal }) => {
           </div>
         </div>
         <div className="waterSouceData-salt">
-          <div className="field">
-            <div>Calcium (ppm)</div>
+          <div className=" column">
             <div>
-              <input type="text" placeholder="0.0" />
+              <div>Final Calcium (ppm)</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
+            </div>
+          </div>
+          <div className=" column">
+            <div>
+              <div>Final Magnesium (ppm)</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
+            </div>
+          </div>
+          <div className=" column">
+            <div>
+              <div>Final Alkalinity as CaCo3</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
+            </div>
+          </div>
+
+          <div className=" column">
+            <div>
+              <div>Final Sulfate (ppm)</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
+            </div>
+          </div>
+          <div className=" column">
+            <div>
+              <div>Final Chloride (ppm)</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
+            </div>
+          </div>
+          <div className=" column">
+            <div>
+              <div>Final Sodium (ppm)</div>
+              <div>
+                <div>0.0</div>
+              </div>
+            </div>
+            <div>
+              <div>Suggested (ppm)</div>
+              <div>0.0</div>
             </div>
           </div>
           <div className="field">
-            <div>Magnesium (ppm)</div>
+            <div>Total Dissolved Solids</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <div>0.0</div>
             </div>
           </div>
-          <div className="field">
-            <div>Choose Bicarbonate or Alkalinity</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field">
-            <div>Alkaliniy as CaCO3</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field">
-            <div>Sulfate (ppm)</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field">
-            <div>Chloride (ppm)</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field">
-            <div>Sodium (ppm)</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field">
-            <div>Water pH</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
-          </div>
-          <div className="field button">
+          <div className="field button adjustedWaterResults-button">
             <div>
               <button>Confirm and Proceed</button>
             </div>
