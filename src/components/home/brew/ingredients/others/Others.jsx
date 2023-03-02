1cb31262bc872ed07e05bb3489fae245be318ce4
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-02 12:44:38 +0100

    adding others section
    adding others section

--- src/components/home/brew/ingredients/others/Others.jsx
+++ src/components/home/brew/ingredients/others/Others.jsx
@@ -1,11 +1,44 @@
+import "../commonEngredientsModal/commonEngredientsModal.css";
 import "./others.css";
+import { useState } from "react";
 import NavbarIngredients from "../navbarIngredients/NavbarIngredients";
 
 const Others = () => {
+  const [engredients, setEngredients] = useState([
+    {
+      name: "new",
+      description: "new",
+    },
+  ]);
   return (
     <div className="ingredients">
       <NavbarIngredients prew="/yeasts" next="/recipes" />
-      <div>hello</div>
+      <div className="common-ingredients-container">
+        <div className="common-ingredients-container-inside">
+          <div className="common-ingredients-container-inside-section-top">
+            <div className="common-ingredients-container-inside-title">
+              Add a new Ingredients to your beer
+            </div>
+          </div>
+          <div className="common-ingredients-container-inside-section-middle">
+            {engredients.map((engrediet, i) => (
+              <div key={engrediet.name} className="ingredients-others-single">
+                <div> cancel</div>
+
+                <div> {engrediet.name}</div>
+                <div> {engrediet.description}</div>
+                <div> plus</div>
+              </div>
+            ))}
+          </div>
+          <div className="common-ingredients-container-inside-section-bottom">
+            <div className="malts-container-inside-section-bottom-add">
+              <div>Skip and proceed</div>
+            </div>
+            <div>Save and proceed</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
