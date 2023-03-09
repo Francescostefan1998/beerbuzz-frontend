971d01d14204e9ad5a6f37da49de43f660863fb0
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-09 18:01:01 +0100

    adding water diluition

--- src/components/home/brew/ingredients/water/waterSourceData/WaterSourceData.jsx
+++ src/components/home/brew/ingredients/water/waterSourceData/WaterSourceData.jsx
@@ -1,6 +1,24 @@
 import "./waterSourceData.css";
 import { RiCloseLine } from "react-icons/ri";
+import { useDispatch } from "react-redux";
+import { addOriginalAlkalinityAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalCalciumAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalMagnesiumAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalSulfateAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalChlorideAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalSodiumAction } from "../../../../../../redux/actions/originalWater";
+import { addOriginalPhAction } from "../../../../../../redux/actions/originalWater";
+import { useSelector } from "react-redux";
 const WaterSourceData = ({ setModal }) => {
+  const dispatch = useDispatch();
+  const { calcium } = useSelector((state) => state.originalWater);
+  const { magnesium } = useSelector((state) => state.originalWater);
+  const { alkalinity } = useSelector((state) => state.originalWater);
+  const { chloride } = useSelector((state) => state.originalWater);
+  const { sulfate } = useSelector((state) => state.originalWater);
+  const { sodium } = useSelector((state) => state.originalWater);
+  const { ph } = useSelector((state) => state.originalWater);
+
   return (
     <div className="waterSouceData">
       <div className="waterSouceData-modal">
@@ -17,49 +35,87 @@ const WaterSourceData = ({ setModal }) => {
           <div className="field">
             <div>Calcium (ppm)</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={calcium ? `${calcium}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalCalciumAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
             <div>Magnesium (ppm)</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={magnesium ? `${magnesium}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalMagnesiumAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
-            <div>Choose Bicarbonate or Alkalinity</div>
-            <div>
-              <input type="text" placeholder="0.0" />
-            </div>
+            <div>The Alkalinity is expressed in CaCO3</div>
+            <div></div>
           </div>
           <div className="field">
             <div>Alkaliniy as CaCO3</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={alkalinity ? `${alkalinity}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalAlkalinityAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
             <div>Sulfate (ppm)</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={sulfate ? `${sulfate}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalSulfateAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
             <div>Chloride (ppm)</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={chloride ? `${chloride}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalChlorideAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
             <div>Sodium (ppm)</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={sodium ? `${sodium}` : "0.0"}
+                onChange={(e) =>
+                  dispatch(addOriginalSodiumAction(e.target.value))
+                }
+              />
             </div>
           </div>
           <div className="field">
             <div>Water pH</div>
             <div>
-              <input type="text" placeholder="0.0" />
+              <input
+                type="number"
+                placeholder={ph ? `${ph}` : "0.0"}
+                onChange={(e) => dispatch(addOriginalPhAction(e.target.value))}
+              />
             </div>
           </div>
           <div className="field button">
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,12 +4,14 @@ import { createRecipeReducer } from "../reducers/createRecipeReducer";
 import { addStepsRecipeReducer } from "../reducers/addStepsReducer";
 import { addStyleReducer } from "../reducers/addStyleReducer";
 import { originalWaterReducer } from "../reducers/originalWaterReducer";
+import { updateWaterReducer } from "../reducers/updateWaterReducer";
 const mainReducer = {
   recipeIngredient: addProductRecipeReducer,
   recipeSteps: addStepsRecipeReducer,
   createRecipe: createRecipeReducer,
   style: addStyleReducer,
   originalWater: originalWaterReducer,
+  updatedWater: updateWaterReducer,
 };
 
 export const store = configureStore({
