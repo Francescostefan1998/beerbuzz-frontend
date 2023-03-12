f88fc09f3c67797957432079733db5ceced370ed
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-12 12:22:38 +0100

    start styling water
    start styling water

--- src/components/home/brew/ingredients/water/Water.jsx
+++ src/components/home/brew/ingredients/water/Water.jsx
@@ -14,153 +14,158 @@ import { IoIosWater } from "react-icons/io";
 import { TbSalt } from "react-icons/tb";
 import { GiAcid, GiChemicalDrop } from "react-icons/gi";
 import { FaEquals } from "react-icons/fa";
+import NavBar from "../../../navBar/NavBar";
 const Water = () => {
   const [step, setStep] = useState("");
   return (
     <div className="water">
-      <NavbarIngredients prew="/selectstyle" next="/malts" />
-      <div className="water-title mt-4 mb-3">
-        <h2>Water</h2>
-      </div>
-      <div className="water-list">
-        <div
-          className="water-list-element"
-          onClick={() => setStep("waterSourceData")}
-        >
-          <div>
-            <IoWaterOutline className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 1: Source Water Data</h5>
-            </div>
-            <div>Enter Source Water Profile </div>
-          </div>
+      <NavBar />
+      <div className="common-selection-bigger-container">
+        <div className="water-title">
+          <h2>Water</h2>
         </div>
-        {step === "waterSourceData" ? (
-          <WaterSourceData setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("residualAlkalinityTarger")}
-        >
-          <div>
-            <FiTarget className="icon" />
-          </div>
-          <div className="water-list-element-tile">
+        <div className="water-list">
+          <div
+            className="water-list-element"
+            onClick={() => setStep("waterSourceData")}
+          >
             <div>
-              <h5>Step 2: Residual Alkalinity Target</h5>
+              <IoWaterOutline className="icon" />
             </div>
-            <div>Enter your target RA and the water volume to be adjusted</div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 1: Source Water Data</h5>
+              </div>
+              <div>Enter Source Water Profile </div>
+            </div>
           </div>
+          {step === "waterSourceData" ? (
+            <WaterSourceData setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("residualAlkalinityTarger")}
+          >
+            <div>
+              <FiTarget className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 2: Residual Alkalinity Target</h5>
+              </div>
+              <div>
+                Enter your target RA and the water volume to be adjusted
+              </div>
+            </div>
+          </div>
+          {step === "residualAlkalinityTarger" ? (
+            <ResidualAlkalinity setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("souceWaterDiluition")}
+          >
+            <div>
+              <IoIosWater className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 3: Source Water Diluition</h5>
+              </div>
+              <div>Diluite source water with distilled water</div>
+            </div>
+          </div>
+          {step === "souceWaterDiluition" ? (
+            <SourceWaterDiluition setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("saltAdditions")}
+          >
+            <div>
+              <TbSalt className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 4: Salt Additions</h5>
+              </div>
+              <div>Add various salt to boots ion levels</div>
+            </div>
+          </div>
+          {step === "saltAdditions" ? (
+            <SaltAdditions setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("acidAdditions")}
+          >
+            <div>
+              <GiAcid className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 5: Acid Additions</h5>
+              </div>
+              <div>Suggested Acid Additions to help reach your RA target</div>
+            </div>
+          </div>
+          {step === "acidAdditions" ? (
+            <AcidAdditions setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("adjustedWaterResults")}
+          >
+            <div>
+              <FaEquals className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 6: Adjusted Water Results</h5>
+              </div>
+              <div>
+                Compare your results with the suggested ranges for the style
+              </div>
+            </div>
+          </div>
+          {step === "adjustedWaterResults" ? (
+            <AdjustedWaterResults setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
+          <div
+            className="water-list-element"
+            onClick={() => setStep("spargeWaterAcidification")}
+          >
+            <div>
+              <GiChemicalDrop className="icon" />
+            </div>
+            <div className="water-list-element-tile">
+              <div>
+                <h5>Step 7: Sparge Water Acidification</h5>
+              </div>
+              <div>
+                Optional: if sparge water is being treated separately from mash
+                water
+              </div>
+            </div>
+          </div>
+          {step === "spargeWaterAcidification" ? (
+            <SpargeWaterAcidification setModal={setStep} />
+          ) : (
+            <div></div>
+          )}
         </div>
-        {step === "residualAlkalinityTarger" ? (
-          <ResidualAlkalinity setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("souceWaterDiluition")}
-        >
-          <div>
-            <IoIosWater className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 3: Source Water Diluition</h5>
-            </div>
-            <div>Diluite source water with distilled water</div>
-          </div>
-        </div>
-        {step === "souceWaterDiluition" ? (
-          <SourceWaterDiluition setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("saltAdditions")}
-        >
-          <div>
-            <TbSalt className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 4: Salt Additions</h5>
-            </div>
-            <div>Add various salt to boots ion levels</div>
-          </div>
-        </div>
-        {step === "saltAdditions" ? (
-          <SaltAdditions setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("acidAdditions")}
-        >
-          <div>
-            <GiAcid className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 5: Acid Additions</h5>
-            </div>
-            <div>Suggested Acid Additions to help reach your RA target</div>
-          </div>
-        </div>
-        {step === "acidAdditions" ? (
-          <AcidAdditions setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("adjustedWaterResults")}
-        >
-          <div>
-            <FaEquals className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 6: Adjusted Water Results</h5>
-            </div>
-            <div>
-              Compare your results with the suggested ranges for the style
-            </div>
-          </div>
-        </div>
-        {step === "adjustedWaterResults" ? (
-          <AdjustedWaterResults setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
-        <div
-          className="water-list-element"
-          onClick={() => setStep("spargeWaterAcidification")}
-        >
-          <div>
-            <GiChemicalDrop className="icon" />
-          </div>
-          <div className="water-list-element-tile">
-            <div>
-              <h5>Step 7: Sparge Water Acidification</h5>
-            </div>
-            <div>
-              Optional: if sparge water is being treated separately from mash
-              water
-            </div>
-          </div>
-        </div>
-        {step === "spargeWaterAcidification" ? (
-          <SpargeWaterAcidification setModal={setStep} />
-        ) : (
-          <div></div>
-        )}
       </div>
     </div>
   );
