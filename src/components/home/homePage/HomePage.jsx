710d71bea788644281fa554f61ca2dd6d6acbbc5
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-13 10:14:34 +0100

    home page styling
    home page styling

--- src/components/home/homePage/HomePage.jsx
+++ src/components/home/homePage/HomePage.jsx
@@ -1,82 +1,115 @@
+import "../brew/brewMainPage/brewMainPage.css";
 import "./homePage.css";
 import NavBar from "../navBar/NavBar";
+import { Container, Row, Col } from "react-bootstrap";
 import { ImList2 } from "react-icons/im";
 import { BsPencilFill } from "react-icons/bs";
 import { GiHops } from "react-icons/gi";
 import { GiWheat } from "react-icons/gi";
 import { AiOutlineSetting } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import BrewInsideContainer from "../brew/brewMainPage/brewInsideContainer/BrewInsideContainer";
 const HomePage = () => {
   const navigate = useNavigate();
-  let bubbles1 = [];
-  let bubbles2 = [];
-  let bubbles3 = [];
-  let bubbles4 = [];
-  let bubbles5 = [];
-  let bubbles6 = [];
 
-  for (let i = 0; i < 10; i++) {
-    bubbles1.push(
-      <div style={{ left: i * 45 + "px" }} key={i} className="bubble bubble1" />
-    );
-  }
-  for (let i = 0; i < 8; i++) {
-    bubbles2.push(
-      <div style={{ left: i * 77 + "px" }} key={i} className="bubble bubble2" />
-    );
-  }
-  for (let i = 0; i < 12; i++) {
-    bubbles3.push(
-      <div style={{ left: i * 42 + "px" }} key={i} className="bubble bubble3" />
-    );
-  }
-  for (let i = 0; i < 8; i++) {
-    bubbles4.push(
-      <div style={{ left: i * 67 + "px" }} key={i} className="bubble bubble4" />
-    );
-  }
-  for (let i = 0; i < 10; i++) {
-    bubbles5.push(
-      <div style={{ left: i * 35 + "px" }} key={i} className="bubble bubble5" />
-    );
-  }
-  for (let i = 0; i < 12; i++) {
-    bubbles6.push(
-      <div style={{ left: i * 52 + "px" }} key={i} className="bubble bubble3" />
-    );
-  }
   return (
     <div className="homePage">
       <NavBar selectedSection={"Home"} />
       <div className="homePage-container">
-        <div className="homePage-main" onClick={() => navigate("/recipes")}>
+        <div
+          className="homePage-main homepage-sticky-to-top"
+          onClick={() => navigate("/recipes")}
+        >
           <ImList2 className="homePage-main-icon iconList" />
           <BsPencilFill className="homePage-main-icon iconPen" />
           <GiHops className="homePage-main-icon iconHops" />
           <GiWheat className="homePage-main-icon iconWheat" />
-          <h1>Recipes</h1>
+          <h1 className="mb-4">Recipes</h1>
+          <Container>
+            <Row className={`brewMainPage-row block`}>
+              <Col
+                className={`brewMainPage-section block`}
+                onClick={() => navigate("/selectstyle")}
+              >
+                <img
+                  src="https://i.etsystatic.com/20492529/r/il/442b0f/3153997959/il_fullxfull.3153997959_esj0.jpg"
+                  alt="style"
+                />
+                <h1>My Favourite</h1>
+                <div className="brewMainPage-step">1</div>
+              </Col>
+              <Col
+                className={`brewMainPage-section block`}
+                onClick={() => navigate("/ingredients")}
+              >
+                <img
+                  src="https://th.bing.com/th/id/OIP.CPsBCeMpHQ57V3weDLMbMQHaJg?pid=ImgDet&rs=1"
+                  alt="ingredients"
+                />
+                <h1>All Recipes</h1>
+                <div className="brewMainPage-step">2</div>
+              </Col>
+            </Row>
+            <Row className={`brewMainPage-row block`}>
+              <Col className={`brewMainPage-section block`}>
+                <img
+                  src="https://th.bing.com/th/id/OIP._xg6a-Ol2g2iwet6Ub3-mwHaHa?pid=ImgDet&rs=1"
+                  alt="grain"
+                />
+                <h1>Drafts</h1>
+                <div className="brewMainPage-step">3</div>
+              </Col>
+            </Row>
+          </Container>
         </div>
         <div className="homePage-main" onClick={() => navigate("/brew")}>
           <div className="homePage-main-title">
-            <h1>Make a New Beer</h1>
+            <h1 className="mb-4">Make a New Beer</h1>
           </div>
-
-          <div className="homePage-main-liquid">
-            <div className="homePage-main-liquid-baal1"></div>
-            <div className="homePage-main-liquid-baal2"></div>
-            <div className="homePage-main-liquid-baal3"></div>
-
-            {bubbles1}
-            {bubbles2}
-            {bubbles3}
-            {bubbles4}
-            {bubbles5}
-            {bubbles6}
-          </div>
+          <BrewInsideContainer block={"block"} />
         </div>
-        <div className="homePage-main" onClick={() => navigate("/instruction")}>
+        <div
+          className="homePage-main homepage-sticky-to-top"
+          onClick={() => navigate("/instruction")}
+        >
           <AiOutlineSetting className="homePage-main-icon iconSetting" />
-          <h1>Instructions</h1>
+          <h1 className="mb-4">Instructions</h1>
+          <Container>
+            <Row className={`brewMainPage-row block`}>
+              <Col
+                className={`brewMainPage-section block`}
+                onClick={() => navigate("/selectstyle")}
+              >
+                <img
+                  src="https://thumbs.dreamstime.com/z/grande-insieme-d-annata-degli-oggetti-della-birra-vari-tipi-di-vetri-e-tazze-vecchio-barilotto-legno-luppolo-bottiglia-latta-apri-150652832.jpg"
+                  alt="style"
+                />
+                <h1>How to Start</h1>
+                <div className="brewMainPage-step">1</div>
+              </Col>
+              <Col
+                className={`brewMainPage-section block`}
+                onClick={() => navigate("/ingredients")}
+              >
+                <img
+                  src="https://thumbnails.yayimages.com/1600/2/243/22439ea.jpg"
+                  alt="ingredients"
+                />
+                <h1>Presonalize Recipe</h1>
+                <div className="brewMainPage-step">2</div>
+              </Col>
+            </Row>
+            <Row className={`brewMainPage-row block`}>
+              <Col className={`brewMainPage-section block`}>
+                <img
+                  src="https://i.pinimg.com/originals/ec/2b/57/ec2b5789fa7128f5f178aac22c15fc16.jpg"
+                  alt="grain"
+                />
+                <h1>Help</h1>
+                <div className="brewMainPage-step">3</div>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
     </div>
