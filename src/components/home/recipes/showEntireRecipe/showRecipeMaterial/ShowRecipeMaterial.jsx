8dfc0c3344341675276c424c63b9693ef189a9df
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-14 15:52:10 +0100

    adding material
    adding material

--- src/components/home/recipes/showEntireRecipe/showRecipeMaterial/ShowRecipeMaterial.jsx
+++ src/components/home/recipes/showEntireRecipe/showRecipeMaterial/ShowRecipeMaterial.jsx
@@ -2,8 +2,36 @@ import "./showRecipeMaterial.css";
 
 const ShowRecipeMaterial = ({ body }) => {
   return (
-    <dir>
-      <div> hello</div>
+    <dir className="showRecipeMaterial">
+      <h2>Material</h2>
+      <h4>Salts</h4>
+      {body.salts.map((salt, i) => (
+        <div key={salt.name}>
+          {salt.name}: {salt.amount}g
+        </div>
+      ))}
+      <h4>Malts</h4>
+      {body.malts.map((malt, i) => (
+        <div key={malt.nameId}>
+          {malt.nameId}: {malt.amount}kg
+        </div>
+      ))}
+      <h4>Hops</h4>
+      {body.hops.map((malt, i) => (
+        <div key={malt.nameId}>
+          {malt.nameId}: {malt.amount}kg
+        </div>
+      ))}
+      <h4>Yeasts</h4>
+      {body.yeasts.map((malt, i) => (
+        <div key={malt.nameId}>
+          {malt.nameId}: {malt.amount}kg
+        </div>
+      ))}
+      <h4>Others</h4>
+      {body.others.map((malt, i) => (
+        <div key={malt.nameId}>{malt.nameId}</div>
+      ))}
     </dir>
   );
 };
