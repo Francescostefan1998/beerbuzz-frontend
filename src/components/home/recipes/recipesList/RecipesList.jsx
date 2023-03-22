c128b897dec141d185dc518704e5a14fca94e415
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-22 11:01:13 +0100

    add to favourite working
    add to favourite working

--- src/components/home/recipes/recipesList/RecipesList.jsx
+++ src/components/home/recipes/recipesList/RecipesList.jsx
@@ -13,15 +13,17 @@ import ShowEntireRecipe from "../showEntireRecipe/ShowEntireRecipe";
 import BottomBar from "../../../bottomBar/BottomBar";
 import { useNavigate } from "react-router-dom";
 import SideBarLeft from "../../brew/sideBarLeft/SideBarLeft";
-
+import { HiDotsHorizontal } from "react-icons/hi";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsFillPencilFill } from "react-icons/bs";
 import { useEffect } from "react";
 const RecipesList = () => {
   const url = process.env.REACT_APP_BE_URL;
-
+  const [refres, setRefresh] = useState("");
+  const [dropDown, openDropDown] = useState(false);
   const [myRecipes, setMyRecipes] = useState([]);
   const [recipeSelected, setRecipeSelected] = useState("");
   const navigate = useNavigate();
-  console.log(myRecipes);
   const userIdRec = localStorage.getItem("userId");
   const fetchRecipes = async (userId) => {
     const res = await fetch(`${url}/recipes/${userId}`);
@@ -30,15 +32,29 @@ const RecipesList = () => {
   };
   useEffect(() => {
     fetchRecipes(userIdRec);
-  }, []);
+    console.log("Refresh");
+  }, [dropDown, refres]);
+
   const deleteSingleRecipe = async (itemId) => {
     try {
       const response = await axios.delete(`${url}/recipes/${itemId}`);
       console.log(`Item with ID ${itemId} deleted successfully.`);
       setRecipeSelected("");
       fetchRecipes(userIdRec);
+      navigate("/recipes");
     } catch (error) {
       console.error(`Error deleting item with ID ${itemId}: ${error.message}`);
+    }
+  };
+  const addToFavourite = async (itemId, value) => {
+    try {
+      const response = await axios.put(`${url}/recipes/${itemId}`, {
+        favourite: value,
+      });
+      console.log(`Item with ID ${itemId} updated successfully.`);
+      setRefresh(itemId + value);
+    } catch (error) {
+      console.error(`Error updating item with ID ${itemId}: ${error.message}`);
     }
   };
   return (
@@ -59,13 +75,63 @@ const RecipesList = () => {
         ) : (
           <div className="showEnt-recipe-title">
             <div>
-              <h1>Beer</h1>
+              <h1>BeerBuzz</h1>
             </div>
             <div>
-              <BsTrashFill
-                className="showEnt-recipe-title-icon"
-                onClick={(e) => deleteSingleRecipe(recipeSelected)}
-              />
+              <div className="shownEnt-recipe-dropdown-outside">
+                <div
+                  className="settingDropdown-close shownEnt-recipe-dropdown-outside"
+                  onClick={() => {
+                    dropDown === true
+                      ? openDropDown(false)
+                      : openDropDown(true);
+                  }}
+                >
+                  <HiDotsHorizontal className="settingDropdown-close-icon" />{" "}
+                </div>
+                {dropDown && (
+                  <div className="shownEnt-recipe-dropdown-inside">
+                    <div className="relative-line-dropDown">
+                      <div className="relative-line-dropDown-triangle"></div>
+                      <div className="section-dropdown">Add to favourite</div>
+                      <div className="section-dropdown">
+                        {myRecipes &&
+                        myRecipes.find((item) => item._id === recipeSelected)
+                          .favourite &&
+                        myRecipes.find((item) => item._id === recipeSelected)
+                          .favourite === "yes" ? (
+                          <AiFillHeart
+                            className="showEnt-recipe-title-icon section-dropdown margin-left-1vh"
+                            onClick={() => addToFavourite(recipeSelected, "no")}
+                          />
+                        ) : (
+                          <AiOutlineHeart
+                            className="showEnt-recipe-title-icon section-dropdown margin-left-1vh"
+                            onClick={() =>
+                              addToFavourite(recipeSelected, "yes")
+                            }
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <div className="relative-line-dropDown">
+                      <div className="section-dropdown">Delete recipe</div>
+                      <BsTrashFill
+                        className="showEnt-recipe-title-icon section-dropdown"
+                        onClick={(e) => deleteSingleRecipe(recipeSelected)}
+                      />
+                    </div>
+                    <div className="relative-line-dropDown">
+                      <div className="section-dropdown">Add a Note</div>
+                      <BsFillPencilFill
+                        className="showEnt-recipe-title-icon section-dropdown"
+                        onClick={() => openDropDown(false)}
+                      />
+                    </div>
+                    <div className="relative-line-dropDown"></div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         )}
